9bda05b9276ceda0b931f6c23460d9ec31a5d3a1
zhangqiang01 <zhangqiang01@lianjia.com>
2017-05-15 17:03:02 +0800

    update

--- MyDemo/compile.sh
+++ MyDemo/compile.sh
@@ -2,5 +2,11 @@
 echo "I'm shell script"
 #cd ../
 echo `pwd`
-gradle -v
+#gradle -v
+#adb mount -o remount /
+#adb shell mkdir sdcard/plu | adb push -p /c/Users/zhangqiang/Desktop/newlink-10-10-101.jar sdcard/plu/newlink.jar | adb shell am force-stop com.homelink.newlink | adb shell am start -n com.homelink.newlink/com.homelink.newlink.ui.app.SplashScreenActivity
+#adb shell mkdir sdcard/plu | adb push -p /c/Users/zhangqiang/Desktop/newlink-10-10-101.jar sdcard/plu/newlink.jar
+#adb shell mkdir sdcard/plu
+adb shell rm -rf sdcard/plu
+echo $?
 #gradle -p /d/learn/TheDemo/MyDemo assembleBaiduDebug
--- MyDemo/src/main/java/com/demo/activity/TestViewGroupActivity.java
+++ MyDemo/src/main/java/com/demo/activity/TestViewGroupActivity.java
@@ -3,11 +3,19 @@ package com.demo.activity;
 import android.os.Bundle;
 
 import com.demo.R;
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/7/9.
  */
 public class TestViewGroupActivity extends BaseActivity{
+
+    String[] titleArray = new String[]{"商圈排名","大区排名","城市排名"};
+    private CommonTabLayout mTabLayout;
+    private final ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +24,32 @@ public class TestViewGroupActivity extends BaseActivity{
     @Override
     public void setView() {
         setContentView(R.layout.activity_viewgroup);
+        mTabLayout = (CommonTabLayout)findViewById(R.id.tab_layout);
+        for(int i=0; i<3; i++){
+            TextTabEntity entity = new TextTabEntity(titleArray[i]);
+            mTabEntities.add(entity);
+        }
+        mTabLayout.setTabData(mTabEntities);
+    }
+
+    private class TextTabEntity implements CustomTabEntity{
+
+        private String title;
+
+        public TextTabEntity(String title) {
+            this.title = title;
+        }
+
+        @Override public String getTabTitle() {
+            return title;
+        }
+
+        @Override public int getTabSelectedIcon() {
+            return 0;
+        }
+
+        @Override public int getTabUnselectedIcon() {
+            return 0;
+        }
     }
 }
