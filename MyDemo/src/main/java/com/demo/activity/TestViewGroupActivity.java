e805f7a974d52b09be17d94e6e0a11aa40344ce0
zhangqiang01 <zhangqiang01@lianjia.com>
2017-06-02 19:56:16 +0800

    js demo

--- MyDemo/src/main/java/com/demo/activity/TestViewGroupActivity.java
+++ MyDemo/src/main/java/com/demo/activity/TestViewGroupActivity.java
@@ -2,9 +2,13 @@ package com.demo.activity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
 import com.demo.R;
+import com.demo.js.JsManager;
 import com.flyco.tablayout.CommonTabLayout;
 import com.flyco.tablayout.listener.CustomTabEntity;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
@@ -13,12 +17,21 @@ import java.util.ArrayList;
 public class TestViewGroupActivity extends BaseActivity{
 
     String[] titleArray = new String[]{"商圈排名","大区排名","城市排名"};
+    private TextView tv;
     private CommonTabLayout mTabLayout;
     private final ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tv = (TextView) findViewById(R.id.tv);
+        try {
+            InputStream in = getAssets().open("expression.js");
+            int result = (int) JsManager.getInstance().invoke(in, "merge", new Object[]{2,3});
+            tv.setText(result + "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
---
+++ jsmanager/src/main/java/com/seasonfif/JsManager/JsManager.java
@@ -0,0 +1,63 @@
+package com.seasonfif.JsManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class JsManager {
+
+  private static volatile JsManager instance;
+
+  private ScriptEngineManager scriptEngineManager;
+  private ScriptEngine engine;
+
+  public static JsManager getInstance() {
+    if (instance == null){
+      synchronized (JsManager.class){
+        if (instance == null){
+          instance = new JsManager();
+        }
+      }
+    }
+    return instance;
+  }
+
+  private JsManager(){
+    scriptEngineManager = new ScriptEngineManager();
+    engine = scriptEngineManager.getEngineByName("javascript");
+  }
+
+  public Object invoke(InputStream in, String function, Object...args) {
+    Reader scriptReader = null;
+    try {
+      scriptReader = new InputStreamReader(in, "utf-8");
+      engine.eval(scriptReader);
+      if (engine instanceof Invocable){
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(function, args);
+      }
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    } catch (ScriptException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    } finally {
+      if (scriptReader != null){
+        try {
+          scriptReader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    return engine;
+  }
+}
